76d0bfb1f2aa6f5ddd2b98707f3494790bcb7382
njznn <88048473+njznn@users.noreply.github.com>
2024-07-29 09:14:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 
 int main() {
+    //vector of strings:
     vector v;
     vec_init(&v, 5, sizeof(char)); // Initialize vector for int
 
@@ -9,22 +10,18 @@ int main() {
     char *strings2[] = {"aa", "a"};
     double nm  = 3;
     vec_push_back(&v,strings, 6);
-    printf("Address of num: %p\n", (char *)v.data[0]);
-    //printf("Address of num: %p\n", (double *)v.data[0]);
     vec_insert(&v, 0, strings2, 2);
-    printf("Address of num: %p\n", (char *)v.data[0]);
-    //printf("Address of num: %p\n", (char *)strings);
-    //printf("Address of num: %p\n", (double *)&v.data[0]);
-    // Print the vector
     printf("Vector contents:\n");
     print_vector(&v);
-    //vec_delete(&v);
+    vec_delete(&v);
 
+    //vector of doubles:
     vector v1;
     vec_init(&v1, 6, sizeof(double));
     vec_push_back(&v1, &(double[]){1,2,3,4,5}, 5);
     vec_insert(&v1, 0, &(double[]){7,6},3);
     print_vector(&v1);
+    vec_delete(&v1);
 
 
     return 0;
